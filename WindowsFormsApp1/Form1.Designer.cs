64421f0136535aec3c66cd7f9f65ce0a50764f57
P4 <P4@TK_017>
2021-09-15 14:23:30 -0400

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Simpson1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.reactor1label = new System.Windows.Forms.Label();
+            this.reactor2label = new System.Windows.Forms.Label();
+            this.reactor1StateLabel = new System.Windows.Forms.Label();
+            this.reactor2StateLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(191, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(306, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nuclear Reactor Control";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // reactor1label
+            // 
+            this.reactor1label.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reactor1label.Location = new System.Drawing.Point(140, 48);
+            this.reactor1label.Name = "reactor1label";
+            this.reactor1label.Size = new System.Drawing.Size(100, 23);
+            this.reactor1label.TabIndex = 1;
+            this.reactor1label.Text = "Reactor 1";
+            this.reactor1label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // reactor2label
+            // 
+            this.reactor2label.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reactor2label.Location = new System.Drawing.Point(454, 48);
+            this.reactor2label.Name = "reactor2label";
+            this.reactor2label.Size = new System.Drawing.Size(100, 23);
+            this.reactor2label.TabIndex = 2;
+            this.reactor2label.Text = "Reactor 1";
+            this.reactor2label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.reactor2label.Click += new System.EventHandler(this.reactor2label_Click);
+            // 
+            // reactor1StateLabel
+            // 
+            this.reactor1StateLabel.BackColor = System.Drawing.Color.Green;
+            this.reactor1StateLabel.Location = new System.Drawing.Point(116, 82);
+            this.reactor1StateLabel.Name = "reactor1StateLabel";
+            this.reactor1StateLabel.Size = new System.Drawing.Size(146, 117);
+            this.reactor1StateLabel.TabIndex = 3;
+            // 
+            // reactor2StateLabel
+            // 
+            this.reactor2StateLabel.BackColor = System.Drawing.Color.Green;
+            this.reactor2StateLabel.Location = new System.Drawing.Point(431, 82);
+            this.reactor2StateLabel.Name = "reactor2StateLabel";
+            this.reactor2StateLabel.Size = new System.Drawing.Size(146, 117);
+            this.reactor2StateLabel.TabIndex = 4;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.White;
+            this.outputLabel.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(139, 274);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(415, 34);
+            this.outputLabel.TabIndex = 5;
+            this.outputLabel.Text = "Reactors Stable";
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.outputLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(260, 222);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(167, 30);
+            this.startButton.TabIndex = 6;
+            this.startButton.Text = "Start Reactors";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Simpson1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.reactor2StateLabel);
+            this.Controls.Add(this.reactor1StateLabel);
+            this.Controls.Add(this.reactor2label);
+            this.Controls.Add(this.reactor1label);
+            this.Controls.Add(this.label1);
+            this.Name = "Simpson1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label reactor1label;
+        private System.Windows.Forms.Label reactor2label;
+        private System.Windows.Forms.Label reactor1StateLabel;
+        private System.Windows.Forms.Label reactor2StateLabel;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button startButton;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows.Forms;
+
+using System.Threading; //allows the use of Thread.Sleep() 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Simpson1 : Form
+    {
+        public Simpson1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reactor2label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+
+            //change the colour of the state labels 
+
+            reactor1StateLabel.BackColor = Color.Red;
+
+            reactor2StateLabel.BackColor = Color.Red;
+
+
+
+            //change the message in the output label 
+
+            outputLabel.Text = "Meltdown Imminent";
+
+
+        }
+    }
+}
